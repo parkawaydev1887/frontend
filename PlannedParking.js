b1b650459bb9b9a4c995e03ada110ea9f652f584
Parkaway <parkaway1887@gmail.com>
2020-03-12 11:22:27 -0700

    Add files via upload

---
+++ PlannedParking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PlannedParking.css';
+
+class PlannedParking extends React.Component {
+  render() {
+    return(
+      <h1>Upcoming Trips</h1>
+    );
+  }
+}
+export default PlannedParking;
